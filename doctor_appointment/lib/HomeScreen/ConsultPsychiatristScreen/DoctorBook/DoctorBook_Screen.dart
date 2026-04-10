964f4ae3e1137d51db7125d47d715b8a8d9315c0
poojatechturtlesolution <poojakhandhla@gmail.com>
2026-04-10 10:18:54 +0530

    fix: adjust navigation logic in `DoctorBook_Screen`
    Update the `DoctorBook_Screen` to pop an additional route from the navigator stack upon successful action, ensuring the user is returned to the correct preceding screen.

--- doctor_appointment/lib/HomeScreen/ConsultPsychiatristScreen/DoctorBook/DoctorBook_Screen.dart
+++ doctor_appointment/lib/HomeScreen/ConsultPsychiatristScreen/DoctorBook/DoctorBook_Screen.dart
@@ -258,6 +258,7 @@ class _DoctorBookAppointmentScreenState
               onPressed: () {
                 Navigator.pop(context);
                 Navigator.pop(context);
+                Navigator.pop(context);
                 // Navigator.pushReplacement(
                 //   context,
                 //   MaterialPageRoute(
